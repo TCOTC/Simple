ac76fd1ff222a9f3893b87803974e216edabf7dc
Jeffrey Chen <78434827+TCOTC@users.noreply.github.com>
2024-12-14 01:57:27 +0800

    feat(theme): 实现光标所在块的实时追踪功能

--- theme.js
+++ theme.js
@@ -1,9 +1,61 @@
 window.destroyTheme = () => {
+    // 卸载“跟踪当前所在块”的事件监听器
+    bulletCleanup()
 }
 
-// (function() {
-//     const statusElement = document.getElementById("status");
-//     if (statusElement) {
-//         statusElement.style.backgroundColor = "coral"
-//     }
-// })();
+/**
+ * 获得指定块位于的编辑区
+ * @params {HTMLElement}
+ * @return {HTMLElement} 光标所在块位于的编辑区
+ * @return {null} 光标不在块内
+ */
+const getTargetEditor = function(block) {
+    while (block != null && !block.classList.contains('protyle-wysiwyg')) block = block.parentElement;
+    return block;
+};
+
+/**
+ * 获得焦点所在的块
+ * @return {HTMLElement} 光标所在块
+ * @return {null} 光标不在块内
+ */
+const getFocusedBlock = function() {
+    if (document.activeElement.classList.contains('protyle-wysiwyg')) {
+        let block = window.getSelection()?.focusNode?.parentElement; // 当前光标
+        while (block != null && block.dataset.nodeId == null) block = block.parentElement;
+        return block;
+    }
+};
+
+const focusHandler = function() {
+    // 获取当前编辑区
+    let block = getFocusedBlock(); // 当前光标所在块
+    // 当前块已经设置焦点
+    if (block?.classList.contains(`block-focus`)) return;
+
+    // 当前块未设置焦点
+    const editor = getTargetEditor(block); // 当前光标所在块位于的编辑区
+    if (editor) {
+        editor.querySelectorAll(`.block-focus`).forEach((element) => element.classList.remove(`block-focus`));
+        block.classList.add(`block-focus`);
+        // setSelector(block);
+    }
+};
+
+const bulletMain = function() {
+    // 跟踪当前所在块
+    window.addEventListener('mouseup', focusHandler, true);
+    window.addEventListener('keyup', focusHandler, true);
+};
+
+const bulletCleanup = function() {
+    // 移除类名
+    document.querySelectorAll('.block-focus').forEach((element) => element.classList.remove('block-focus'));
+    // 卸载事件监听器
+    window.removeEventListener('mouseup', focusHandler, true);
+    window.removeEventListener('keyup', focusHandler, true);
+};
+
+(async () => {
+    bulletMain();
+})();
